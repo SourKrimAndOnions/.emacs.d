9f529631c96ba9f22cde54bec8c914755be0279b
Karim Vedelkvist <krv@delegate.dk>
2024-12-12 16:19:03 +0100

    removed packages and changed to elpaca

---
+++ early-init.el
@@ -0,0 +1,3 @@
+;; [[file:config.org::*early init][early init:1]]
+(setq package-enable-at-startup nil)
+;; early init:1 ends here
---
+++ lisp/compare-files.el
@@ -0,0 +1,13 @@
+(defun compare-directories-eshell (dir1 dir2)
+  "Compare files in DIR1 and DIR2 and print the files only in DIR1."
+  (let ((files1 (directory-files dir1 nil "^[^.].*"))
+        (files2 (directory-files dir2 nil "^[^.].*")))
+    (cl-set-difference files1 files2 :test 'string=)))
+
+(defun compare-directories-print (dir1 dir2)
+  "Print the result of comparing DIR1 and DIR2 in Eshell."
+  (let ((difference (compare-directories-eshell dir1 dir2)))
+    (if difference
+        (dolist (file difference)
+          (eshell-print (concat file " ")))
+      (eshell/echo "No unique files in the first directory."))))
